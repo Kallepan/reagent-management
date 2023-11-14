76827415781582b367a51e6ba9cf5f26323f0623
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-14 07:49:22 +0000

    Fix date function to return correct ISO format and filter dates not before today

--- frontend/src/app/core/functions/date.function.ts
+++ frontend/src/app/core/functions/date.function.ts
@@ -1,6 +1,8 @@
-export const isoDateFormat = (date: Date| null): string | null => {
+export const isoDateFormat = (date: Date | null): string | null => {
     // Returns date in format YYYY-MM-DD ISO 8601 or null if date is null
     if (!date) return null;
+
+    date.setDate(date.getDate() + 1)
 
     return date.toISOString().split('T')[0];
 }
@@ -10,4 +12,4 @@ export const filterNotBeforeToday = (d: Date | null): boolean => {
     today.setHours(0, 0, 0, 0);
     if (!d) return false;
     return d >= today;
-  }
+}
