42aad062edd9adf78db12837ed7be908f006ad1c
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-14 11:09:14 +0000

    init: pcr module

---
+++ frontend/src/app/modules/pcr/pcr.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PcrRoutingModule } from './pcr-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PcrRoutingModule
+  ]
+})
+export class PcrModule { }
