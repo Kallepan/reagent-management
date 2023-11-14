f1218c5afa16509292cf0f60fec560078f118762
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-14 09:15:46 +0000

    feat: updated angular

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Angular CLI
-npm install -g @angular/cli
+npm install -g @angular/cli@17.0.0
 
 # Install Node Modules
-export NODE_MODULES="eslint typescript @angular/cli"
+export NODE_MODULES="eslint typescript"
 npm install -g ${NODE_MODULES} \
     && npm cache clean --force > /dev/null 2>&1
 
