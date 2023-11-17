e5fc9ed7b41fd769b5d5f125030c41b4c8a23029
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-17 12:24:55 +0000

    cleanup: typo

--- frontend/src/app/modules/bak/bak-routing.module.ts
+++ frontend/src/app/modules/bak/bak-routing.module.ts
@@ -4,17 +4,11 @@ import { LotsListComponent } from './components/lots-list/lots-list.component';
 import { LotsCreateComponent } from './components/lots-create/lots-create.component';
 import { LotsDetailComponent } from './components/lots-detail/lots-detail.component';
 
-// This could be implemented, but it causes page redirection before the lots are loaded.
-// I guess it could be implemented by using a resolver to load the lots before the page is loaded.
-// But I don't think it's worth the effort.
-import { lotExistsGuard } from './guards/lot-exists.guard'; 
-import { isAuthenticated } from '@app/core/guards/auth-guard';
-
 const routes: Routes = [
   { path: 'lots/create', component: LotsCreateComponent, title: 'Create Lot' },
   { path: 'lots/detail/:id', component: LotsDetailComponent, title: 'Lot Detail', }, // TODO: canActivate: [isAuthenticated]
   { path: 'lots', component: LotsListComponent, title: 'Lots' },
-  { path: '', redirectTo: 'lots', pathMatch: 'full'}
+  { path: '', redirectTo: 'lots', pathMatch: 'full' }
 ];
 
 @NgModule({
