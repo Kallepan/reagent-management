80bf2b2ed8d08c06876dca48cd2d7a2d7e08cf1c
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-22 22:29:25 +0100

    increased token duration

--- backend/src/reagent_management_backend/settings.py
+++ backend/src/reagent_management_backend/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 
 # JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     
     'REFRESH_COOKIE': 'refresh_token', # Cookie name. Enables cookies if value is set.
