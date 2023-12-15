749840e0b6a0bdf87e0b333d6c74aa3113c8c173
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-12-15 16:38:11 +0000

    fix: using yarn instead of npm

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install Angular CLI
-npm install -g @angular/cli@latest
+yarn install -g @angular/cli@latest
 
 # Install Node Modules
 export NODE_MODULES="typescript eslint"
-npm install -g ${NODE_MODULES} \
-    && npm cache clean --force > /dev/null 2>&1
+yarn install -g ${NODE_MODULES} \
+    && yarn cache clean --force > /dev/null 2>&1
 
 # Install Python Packages
 pip install -r backend/requirements.txt
 
-# install npm packages
+# install yarn packages
 yarn --cwd frontend install
