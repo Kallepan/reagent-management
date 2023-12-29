b1a793d913aa8b599f50ade2a376e1058173d819
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-12-29 06:37:01 +0000

    fix: angular cli not installed

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install Angular CLI
-yarn install -g @angular/cli@latest
+npm install -g @angular/cli@latest
 
 # Install Node Modules
 export NODE_MODULES="typescript eslint"
-yarn install -g ${NODE_MODULES} \
+npm install -g ${NODE_MODULES} \
     && yarn cache clean --force > /dev/null 2>&1
 
 # Install Python Packages
