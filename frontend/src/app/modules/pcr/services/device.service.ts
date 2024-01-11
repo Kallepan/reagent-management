b3e9e8e144785ac4d929fba86e0746e4d58f188a
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-01-11 13:58:05 +0000

    fix: sorting of devices

--- frontend/src/app/modules/pcr/services/device.service.ts
+++ frontend/src/app/modules/pcr/services/device.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, inject } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Device } from '../interfaces/simple';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CustomResponseType } from '@app/core/interfaces/response';
 import { constants } from '@app/core/constants/constants';
 
 @Injectable({
-  providedIn: null
+  providedIn: null,
 })
 export class DeviceService {
   private http = inject(HttpClient);
@@ -19,11 +19,16 @@ export class DeviceService {
       withCredentials: true,
     };
 
-    return this.http.get<CustomResponseType>(constants.APIS.PCR.BASE + '/devices/' + id, httpOptions).pipe(
-      map(resp => {
-        return resp.data as Device;
-      })
-    );
+    return this.http
+      .get<CustomResponseType>(
+        constants.APIS.PCR.BASE + '/devices/' + id,
+        httpOptions,
+      )
+      .pipe(
+        map((resp) => {
+          return resp.data as Device;
+        }),
+      );
   }
 
   getDevices(): Observable<Device[]> {
@@ -34,10 +39,28 @@ export class DeviceService {
       withCredentials: true,
     };
 
-    return this.http.get<CustomResponseType>(constants.APIS.PCR.BASE + '/devices', httpOptions).pipe(
-      map(resp => {
-        return resp.data.results as Device[];
-      })
-    );
+    return this.http
+      .get<CustomResponseType>(
+        constants.APIS.PCR.BASE + '/devices',
+        httpOptions,
+      )
+      .pipe(
+        map((resp) => {
+          return resp.data.results as Device[];
+        }),
+        map((devices) => {
+          // change default order, BeGe01 should be last
+          // InGe should be sorted by name
+          return devices.sort((a, b) => {
+            if (a.name.includes('BeGe')) {
+              return 1;
+            } else if (b.name.includes('BeGe')) {
+              return -1;
+            }
+
+            return a.name.localeCompare(b.name);
+          });
+        }),
+      );
   }
 }
